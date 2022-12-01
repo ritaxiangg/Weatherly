85d71b367c56e8f177aa8f871dc61ce1b73fee64
ritaxiang <85653616+ritaxiang@users.noreply.github.com>
2022-12-01 10:27:33 -0500

    Initial commit

---
+++ app/src/main/java/com/example/weatherapptutorial/clothingSuggestion.java
@@ -0,0 +1,14 @@
+package com.example.weatherapptutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class clothingSuggestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothing_suggestion);
+    }
+}
